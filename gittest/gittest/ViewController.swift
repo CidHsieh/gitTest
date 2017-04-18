77081a6e2db3245e4c7105e9bd46555ff44d68cb
Cid Hsieh <topkofaxion7671@hotmail.com>
2017-04-18 09:21:17 +0800

    print 789

--- gittest/gittest/ViewController.swift
+++ gittest/gittest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("123")
         print("456")
+        print("789")
     }
 
     @IBAction func button(_ sender: UIButton) {
